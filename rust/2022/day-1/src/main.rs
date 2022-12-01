959eb2aab2a61dc78117f502c2629980703c9f06
TechTheAwesome <techtheawesome01@gmail.com>
2022-12-02 01:31:03 +0900

    ADD optimized day 1

--- rust/2022/day-1/src/main.rs
+++ rust/2022/day-1/src/main.rs
@@ -1,20 +1,19 @@
-use std::{str::FromStr, string::ParseError};
+use std::{str::FromStr, string::ParseError, time::Instant};
 
 use lib;
 
 struct Elves {
-    calories: Vec<(usize, Vec<u32>, u32)>,
+    calories: Vec<u32>,
 }
 
 impl Elves {
-    pub fn get_fat(&self) -> &(usize, Vec<u32>, u32) {
+    pub fn get_fat(&self) -> &u32 {
         return self.calories.get(0).unwrap();
     }
-
     pub fn get_fat_sum(&self, top: usize) -> u32 {
         let mut sum = 0;
         for i in 0..top {
-            let (_, _, calories) = self.calories.get(i).unwrap();
+            let calories = self.calories.get(i).unwrap();
             sum += calories;
         }
         sum
@@ -28,23 +27,32 @@ impl FromStr for Elves {
         let mut calories = s
             .trim()
             .split("\n\n")
-            .enumerate()
-            .map(|(i, elf)| {
-                let v: Vec<u32> = elf.split("\n").map(|s| s.parse::<u32>().unwrap()).collect();
-                let acc = v.iter().fold(0, |acc, itm| acc + itm);
-                (i + 1, v, acc)
+            .map(|elf| {
+                let acc = elf
+                    .split("\n")
+                    .fold(0, |acc, itm| acc + itm.parse::<u32>().unwrap());
+                acc
             })
-            .collect::<Vec<(usize, Vec<u32>, u32)>>();
-        calories.sort_by(|a, b| b.2.cmp(&a.2));
+            .collect::<Vec<u32>>();
+        calories.sort_by(|a, b| b.cmp(&a));
         Ok(Elves { calories })
     }
 }
 
 #[tokio::main]
 async fn main() {
+    let now = Instant::now();
     let input = lib::get_input(2022, 1).await;
+    println!("Fetch input taken: {:?} ", now.elapsed());
+    let now = Instant::now();
     let elves = input.parse::<Elves>().unwrap();
-    let (index, _, calories) = elves.get_fat();
-    println!("Elf {} carry most calories: {} ", index, calories);
-    println!("Sum of top 3 elves: {}", elves.get_fat_sum(3));
+    println!("Parse input taken: {:?} ", now.elapsed());
+    let now = Instant::now();
+    let calories = elves.get_fat();
+    println!("Taken: {:?} ", now.elapsed());
+    println!("Elf carry most calories: {} ", calories);
+    let now = Instant::now();
+    let sum = elves.get_fat_sum(3);
+    println!("Taken: {:?} ", now.elapsed());
+    println!("Sum of top 3 elves: {}", sum);
 }
